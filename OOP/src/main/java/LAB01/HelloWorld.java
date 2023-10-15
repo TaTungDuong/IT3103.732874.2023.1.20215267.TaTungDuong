3bc81cc8dafdbbc504f475c5048a482ca92ae66a
TaTungDuong <duong.tt215267@sis.hust.edu.vn>
2023-10-16 00:09:21 +0700

    Redo

--- OOP/src/main/java/LAB01/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package LAB01;
-
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Ta Tung Duong 20215267");
-        System.out.println("Hello World!");
-    }
-}
