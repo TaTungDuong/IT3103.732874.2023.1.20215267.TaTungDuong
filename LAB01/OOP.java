096f5334f34f3a6408b27d3553caa9f3df270b98
TaTungDuong <duong.tt215267@sis.hust.edu.vn>
2023-10-15 12:02:15 +0700

    LAB01

---
+++ LAB01/OOP.java
@@ -0,0 +1,8 @@
+package LAB01;
+
+public class OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ OOP/src/main/java/LAB01/p221.java
@@ -0,0 +1,7 @@
+package LAB01;
+
+public class p221 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
